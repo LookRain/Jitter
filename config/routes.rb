5f7badb785cee12697785b0181602b6642fd2924
LookRain <brantscube@hotmail.com>
2016-05-10 13:08:19 +0800

    added twitter bootstrap via SASS gem. implemented global nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'hahaha/hi'
+  root 'pages#index'
+
+  get '/home' => 'pages#home'
+
+  get '/profile' => 'pages#profile'
+
+  get '/explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
